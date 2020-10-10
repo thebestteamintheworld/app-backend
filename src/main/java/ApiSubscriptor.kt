8f413d4e2af4ee678da476175101732291a71904
andre <Andrew.slock@ro.ru>
2020-10-10 12:13:43 +0300

    fix: wrong json ignore

--- src/main/java/ApiSubscriptor.kt
+++ src/main/java/ApiSubscriptor.kt
@@ -11,38 +11,40 @@ class ApiSubscriptor(private var ws: WebSocket, private var apiAddress: String, 
     private var symbol: String = "EUR/USD:AFX{mm=CFH2}"
     private var keepListening: Boolean = true
 
-//    private lateinit var sub: DXFeedSubscription<Quote>
+    //    private lateinit var sub: DXFeedSubscription<Quote>
     private lateinit var listener: DXFeedEventListener<Quote>
 
     private var markup: Double = 1.0
 
     private lateinit var pointer: DXFeedSubscription<Quote>
 
-     fun startListening() {
-        val feed = DXEndpoint.create().connect(apiAddress).feed
-        val sub: DXFeedSubscription<Quote> = feed.createSubscription(Quote::class.java).apply {
-            addEventListener(DXFeedEventListener<Quote> { events -> for (quote in events){
+    fun startListening() {
+        listener = DXFeedEventListener { events ->
+            for (quote in events) {
                 println(gson.toJson(quote))
                 ws.broadcast(gson.toJson(quote))
                 println(this.symbols)
-            } })
+            }
+        }
+        val feed = DXEndpoint.create().connect(apiAddress).feed
+        val sub: DXFeedSubscription<Quote> = feed.createSubscription(Quote::class.java).apply {
+            addEventListener(listener)
             addSymbols(symbol)
         }
         pointer = sub
     }
 
-     fun stopListening() {
+    fun stopListening() {
         pointer.removeEventListener(listener)
     }
 
-     fun changeSymbols(symbols: List<String>) {
-//        println("${symbols[0]} ${symbols[1]}")
-         pointer.removeSymbols(this.symbols)
+    fun changeSymbols(symbols: List<String>) {
+        pointer.removeSymbols(this.symbols)
         pointer.setSymbols(symbols.map { symbol -> "$symbol:AFX{mm=CFH2}" })
         println(pointer.symbols)
     }
 
-     fun setMarkup(new: Double) {
+    fun setMarkup(new: Double) {
         markup = new
     }
 
